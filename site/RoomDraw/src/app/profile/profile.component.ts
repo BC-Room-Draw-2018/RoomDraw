569d13d10b31101a400ac77d24a5463653f64897
Alex Maese <memaese@hotmail.com>
2020-02-17 16:52:45 -0600

    Add profile component in frontend

---
+++ site/RoomDraw/src/app/profile/profile.component.ts
@@ -0,0 +1,23 @@
+import { GroupService } from '../group.service';
+import { Component, OnInit } from '@angular/core';
+import { Invitations } from '../Invitations';
+import { tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  invitations: Invitations[];
+
+  constructor(
+    private groupService: GroupService
+  ) { }
+
+  ngOnInit() {
+    this.groupService.getInvites().pipe(tap(t => console.log(t)))
+      .subscribe(invitations => this.invitations = invitations);
+  }
+
+}
