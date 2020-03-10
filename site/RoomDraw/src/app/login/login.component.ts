024ce653e4c3409b55827f1c04bcdb46f88a7ae9
Peter Weber <retepsweber@gmail.com>
2020-03-09 20:00:55 -0500

    Fixed backgound image on login page: changed encapsulation in .ts and added background in .css

--- site/RoomDraw/src/app/login/login.component.ts
+++ site/RoomDraw/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  encapsulation: 'ViewEncapsulation.None',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
