523080ac93037bdcab6c728ecf0570b4acb36c59
Peter Weber <retepsweber@gmail.com>
2020-03-24 23:18:19 -0500

    Added some changes to housing component and created image-map component

--- site/RoomDraw/src/app/housing/housing.component.ts
+++ site/RoomDraw/src/app/housing/housing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'app-housing',
 	templateUrl: './housing.component.html',
-	styleUrls: ['./housing.component.css']
+	styleUrls: ['./housing.component.scss']
 })
 export class HousingComponent implements OnInit {
 
