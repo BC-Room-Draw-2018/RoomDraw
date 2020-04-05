d030be9a8e614695c2d29daf191717766ecd42e1
Alex Maese <memaese@hotmail.com>
2020-04-05 15:48:52 -0500

    Change column name in database

--- DBScripts/00_CreateDBAndTables.sql
+++ DBScripts/00_CreateDBAndTables.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS Users(
 	student_id INT NOT NULL,
 	email VARCHAR(256) NOT NULL,
 	password VARCHAR(50) NOT NULL,
-	UserGuid UNIQUEIDENTIFIER NULL, /*Guid used to salt passwords*/
+	salt VARCHAR(64) NULL, /*Guid used to salt passwords*/
 
 	PRIMARY KEY(student_id)
 );	
