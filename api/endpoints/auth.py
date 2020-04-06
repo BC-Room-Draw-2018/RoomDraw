0e45c0521feaf4cadd37e11290679e9a4aaa82ca
Alex Maese <memaese@hotmail.com>
2020-04-05 23:05:52 -0500

    Implement login functionality on api

--- api/endpoints/auth.py
+++ api/endpoints/auth.py
@@ -1,11 +1,51 @@
+import bcrypt
+import json
 import falcon
+import models
+from private.sql import *
 
 import session
 
+def _validate_user(username):
+	if username is None:
+		return False
+	return True
+
+def _validate_password(password):
+	if password is None:
+		return False
+	return True
+
+def _check_password(row, password):
+	return bcrypt.checkpw(password.encode('utf-8'), row.password.encode('utf-8'))
+
 class Login(object):
 	def on_post(self, request, response):
-		pass
+		try:
+			params = json.loads(request.stream.read())
+		except json.JSONDecodeError:
+			raise falcon.HTTPBadRequest()
+
+		username = params.get("username")
+		password = params.get("password")
+
+		if not _validate_user(username):
+			raise falcon.HTTPBadRequest(description="Invalid username")
+
+		if not _validate_password(password):
+			raise falcon.HTTPBadRequest(description="Invalid password")
+
+		with sql() as db_session:
+			user = db_session.query(models.User).filter((models.User.email==username) | (models.User.email==username+"@ravens.benedictine.edu")).first()
+			if user is None:
+				response.status = falcon.HTTP_NO_CONTENT
+				return
+			if _check_password(user, password):
+				sess = session.create_session(user.student_id)
+				response.media = {"token": sess.token, "success": 1}
+			else:
+				response.media = {"token": "", "success": 0}
 
 class Logout(object):
 	def on_post(self, request, response):
-		session.destroy_session(self.session_token)
+		session.destroy_session(self.session)
--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,11 +1,13 @@
 from models.student import Student
+from models.user import User
 from models.dorms import Dorm
 from models.group import Group, Invitation
 from models.rooms import Room
 from models.wishlist import GroupWishlist, StudentWishlist
 
 __all__ = [
-	'Student'
+	'Student',
+	'User',
 	'Dorm',
 	'Group',
 	'Invitation',
